b82bf924014024a44c7b778686447aa159bfb6c0
DeathByDenim <jarno@jarno.ca>
2014-11-06 21:12:47 -0500

    Works for matchmaker. Bye bye secondary colour.

--- ui/mods/dFavouriteColour/dFavouriteColour_new_game.js
+++ ui/mods/dFavouriteColour/dFavouriteColour_new_game.js
@@ -1,9 +1,7 @@
 (function() {
 
 	var myprimarycolour = undefined;
-	var mysecondarycolour = undefined;
 	var myaltprimarycolour = undefined;
-	var myaltsecondarycolour = undefined;
 	var infinite_loop_prevent_counter = 0;
 	var previoussecondarycolour = undefined;
 
@@ -14,12 +12,8 @@
 	// Retrieve the favourite colours from the settings if they exist.
 	if(!_.isUndefined(api.settings.isSet('ui','dFavouriteColour_primary', true)) && api.settings.value('ui','dFavouriteColour_primary') in dFavouriteColour_colourtable)
 		myprimarycolour = dFavouriteColour_colourtable[api.settings.value('ui','dFavouriteColour_primary')];
-	if(!_.isUndefined(api.settings.isSet('ui','dFavouriteColour_secondary', true)) && api.settings.value('ui','dFavouriteColour_secondary') in dFavouriteColour_colourtable)
-		mysecondarycolour = dFavouriteColour_colourtable[api.settings.value('ui','dFavouriteColour_secondary')];
 	if(!_.isUndefined(api.settings.isSet('ui','dFavouriteColour_primary_alternative', true)) && api.settings.value('ui','dFavouriteColour_primary_alternative') in dFavouriteColour_colourtable)
 		myaltprimarycolour = dFavouriteColour_colourtable[api.settings.value('ui','dFavouriteColour_primary_alternative')];
-	if(!_.isUndefined(api.settings.isSet('ui','dFavouriteColour_secondary_alternative', true)) && api.settings.value('ui','dFavouriteColour_secondary_alternative') in dFavouriteColour_colourtable)
-		myaltsecondarycolour = dFavouriteColour_colourtable[api.settings.value('ui','dFavouriteColour_secondary_alternative')];
 
 	// Manually choosing a colour should disable this mod temporarily.
 	var databindstring = $('.slot-color-primary-item').attr('data-bind');
@@ -30,57 +24,17 @@
 	handlers.players = function (payload, force) {
 		oldhandlersplayers(payload, force);
 		
-		if(infinite_loop_prevent_counter > 40)
-		{
+		if(infinite_loop_prevent_counter > 40) {
 			console.log("Infinite loop detected");
 			model.dFavouriteColour_enabled = false;
 			return;
 		}
 
 		if(model.dFavouriteColour_enabled) {
-			var myslot = undefined;
-
-			var armies = model.armies();
-			for(var i = 0; i < armies.length; i++) {
-				var slots = armies[i].slots();
-				for(var j = 0; j < slots.length; j++) {
-					if(slots[j].playerName() == myuberDisplayName) {
-						myslot = slots[j];
-						break;
-					}
-				}
-				if(!_.isUndefined(myslot))
-					break;
-			}
-
-			if(!_.isUndefined(myslot)) {
-				if(!_.isUndefined(myprimarycolour) && myslot.primaryColor() === myprimarycolour.colour) {
-					if(!_.isUndefined(mysecondarycolour) && myslot.secondaryColor() !== mysecondarycolour.colour && previoussecondarycolour !== myslot.secondaryColor()) {
-						previoussecondarycolour = myslot.secondaryColor();
-						model.send_message('next_secondary_color');
-						infinite_loop_prevent_counter++;
-					}
-				}
-				else if(!_.isUndefined(myaltprimarycolour) && myslot.primaryColor() === myaltprimarycolour.colour) {
-					if(!_.isUndefined(myprimarycolour) && !model.colors()[myprimarycolour.index].taken) {
-						model.send_message('set_primary_color_index', myprimarycolour.index);
-					}
-					else
-					{
-						if(!_.isUndefined(myaltsecondarycolour) && myslot.secondaryColor() !== myaltsecondarycolour.colour && previoussecondarycolour !== myslot.secondaryColor()) {
-							previoussecondarycolour = myslot.secondaryColor();
-							model.send_message('next_secondary_color');
-							infinite_loop_prevent_counter++;
-						}
-					}
-				}
-				else {
-					if(!_.isUndefined(myaltprimarycolour) )
-						model.send_message('set_primary_color_index', myaltprimarycolour.index);
-					if(!_.isUndefined(myprimarycolour))
-						model.send_message('set_primary_color_index', myprimarycolour.index);
-				}
-			}
+			if(!_.isUndefined(myaltprimarycolour) )
+				model.send_message('set_primary_color_index', myaltprimarycolour.index);
+			if(!_.isUndefined(myprimarycolour))
+				model.send_message('set_primary_color_index', myprimarycolour.index);
 		}
 	}
 
--- ui/mods/dFavouriteColour/dFavouriteColour_settings.js
+++ ui/mods/dFavouriteColour/dFavouriteColour_settings.js
@@ -1,41 +1,10 @@
 (function() {
-
-	primaryChanged = function(newValue) {
-		var colours = [];
-		if(newValue in dFavouriteColour_colourtable) {
-			colours = dFavouriteColour_colourtable[newValue].secondary_colour.slice();
-		}
-		colours.unshift('RANDOM');
-
-		api.settings.definitions.ui.settings.dFavouriteColour_secondary.options=colours;
-		model.settingDefinitions(api.settings.definitions);
-	}
-
-	primaryAlternativeChanged = function(newValue) {
-		var colours = [];
-		if(newValue in dFavouriteColour_colourtable) {
-			colours = dFavouriteColour_colourtable[newValue].secondary_colour.slice();
-		}
-		colours.unshift('RANDOM');
-
-		api.settings.definitions.ui.settings.dFavouriteColour_secondary_alternative.options=colours;
-		model.settingDefinitions(api.settings.definitions);
-	}
-
 	_.extend(api.settings.definitions.ui.settings, {
 
 		dFavouriteColour_primary: {
 			title: 'Favourite primary colour',
 			type: 'select',
 			options: ['RANDOM', 'GREEN', 'ORANGE', 'PURPLE', 'BLACK', 'WHITE', 'LIGHT BLUE', 'RED', 'PINK', 'DARK BLUE', 'YELLOW', 'BROWN'],
-			default: 'RANDOM',
-			callback: primaryChanged
-		},
-
-		dFavouriteColour_secondary: {
-			title: 'Favourite secondary colour',
-			type: 'select',
-			options: ['RANDOM'],
 			default: 'RANDOM'
 		},
 
@@ -43,27 +12,15 @@
 			title: 'Favourite primary colour alternative',
 			type: 'select',
 			options: ['RANDOM', 'GREEN', 'ORANGE', 'PURPLE', 'BLACK', 'WHITE', 'LIGHT BLUE', 'RED', 'PINK', 'DARK BLUE', 'YELLOW', 'BROWN'],
-			default: 'RANDOM',
-			callback: primaryAlternativeChanged
-		},
-
-		dFavouriteColour_secondary_alternative: {
-			title: 'Favourite secondary colour alternative',
-			type: 'select',
-			options: ['RANDOM'],
 			default: 'RANDOM'
 		}
 	});
-
-	primaryChanged(api.settings.value('ui','dFavouriteColour_primary'));
-	primaryAlternativeChanged(api.settings.value('ui','dFavouriteColour_primary_alternative'));
+	model.settingDefinitions(api.settings.definitions);
 
 	$('.ui').children().append(
 		'                            <div class="sub-group">\n' +
 		'                                <div class="option" data-bind="template: { name: \'setting-template\', data: $root.settingsItemMap()[\'ui.dFavouriteColour_primary\'] }"></div>\n' +
-		'                                <div class="option" data-bind="template: { name: \'setting-template\', data: $root.settingsItemMap()[\'ui.dFavouriteColour_secondary\'] }"></div>\n' +
 		'                                <div class="option" data-bind="template: { name: \'setting-template\', data: $root.settingsItemMap()[\'ui.dFavouriteColour_primary_alternative\'] }"></div>\n' +
-		'                                <div class="option" data-bind="template: { name: \'setting-template\', data: $root.settingsItemMap()[\'ui.dFavouriteColour_secondary_alternative\'] }"></div>\n' +
 		'                            </div>\n'
 	);
 
